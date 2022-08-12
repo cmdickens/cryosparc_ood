d716fe24944824f82c38deefd641fa78eba155df
Charles Michael Dickens <cmdickens@mgt3.cluster>
2022-08-12 10:31:26 -0500

    updating template

--- template/after.sh
+++ template/after.sh
@@ -2,7 +2,7 @@
 echo "Waiting for cryoSPARC server to open host:port ${host}:${port}..."
 echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "${host}:${port}" 39000; then
-  sleep 120
+  sleep 150
   echo "Discovered cryoSPARC server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
@@ -11,4 +11,3 @@ else
   pkill -P ${SCRIPT_PID}
   clean_up 1
 fi
-sleep 2
