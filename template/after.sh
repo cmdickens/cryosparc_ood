77e394cc26edfe095daca1092f643917568c5d86
Charles Michael Dickens <cmdickens@mgt3.cluster>
2023-08-23 17:24:45 -0500

    updating app to support using 1 gpu

--- template/after.sh
+++ template/after.sh
@@ -2,9 +2,9 @@
 echo "Waiting for cryoSPARC server to open host:port ${host}:${port}..."
 echo "TIMING - Starting wait at: $(date)"
 if wait_until_port_used "${host}:${port}" 39000; then
-  sleep 150
   echo "Discovered cryoSPARC server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
+  sleep 150
 else
   echo "Timed out waiting for cryoSPARC server to open host port ${host}:${port}!"
   echo "TIMING - Wait ended at: $(date)"
